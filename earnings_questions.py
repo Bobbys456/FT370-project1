9257766b7a647df19ba6b76191a43b087c39bdff
Bobby Sullivan <bobbysullivan61@gmail.com>
2023-10-20 13:27:13 -0400

    it finally works on the apple netflix and cvs earnings the problem was that \s encompases newlines and tabs as well as normal space I had to just use a literal space character in the regex

--- earnings_questions.py
+++ earnings_questions.py
@@ -1,50 +1,62 @@
-import re
-import pandas as pd
+def getQAs(company):
+    import re
+    import pandas as pd
 
-with open('data\CORRECTED TRANSCRIPT_ Apple, Inc.(AAPL-US), Q2 2023 Earnings Call, 4-May-2023 5_00 PM ET.txt', 'r') as file: 
-    text = file.read()
+    with open('data\\'+ company +'.txt', 'r') as file: 
+        text = file.read()
 
-#create end of block to signify end of speech blocks for pattern recognition
-text = re.sub(r'\.{3,}(?![\\n\\.])', 'EndOfBlock', text)
-text = re.sub(r'(?<=\s{10})(\d+)(?=\n)', 'EndOfBlock', text)
+    #create end of block to signify end of speech blocks for pattern recognition
+    text = re.sub(r'\.{4,}(?![\\n\\.])', 'EndOfBlock\n\n\n\n', text)
+    text = re.sub(r'(?<=\s{10})(\d+)(?=\n)', 'EndOfBlock\n\n\n', text)
 
-#remove page end info string beacause it is not important
-text = re.sub(r'1-877-FACTSET www.callstreet.com[\s\S]{0,700}\d\d\d\d', '', text)
+    #remove page end info string beacause it is not important
+    text = re.sub(r'1-877-FACTSET www.callstreet.com[\s\S]{0,700}\d\d\d\d', '', text)
 
-#remove the endof block tags misplaces at the end of pages rather than at the end of speech blocks 
-text = re.sub(r'\s{10,}(EndOfBlock)', '', text)
+    #remove the endof block tags misplaces at the end of pages rather than at the end of speech blocks 
+    text = re.sub(r'\s{10,}(EndOfBlock)', '', text)
 
+    #Removes everything that isnt questions and answers
+    text = re.sub(r'[\s\S]+QUESTION AND ANSWER SECTION', '\n\n\n\n\n\n', text)
 
-
-with open('data/out.txt', 'w') as file: 
-    file.write(text)
-
-
-with open('data/out.txt') as file: 
-    text = file.read()
-
-#\n\n(\w+\s[\w\s.]+\n)([\w\s-&/]+),([\w\s-&]+,\s[\w.]+)\s+(A\n)([\s\S]+?)(EndOfBlock)
-#old regex  : (\sA\n)([\s\S]+?)(EndOfBlock)
-
-
-
-answers = re.findall(r'\n\n(\w+\s[\w\s.]+\n)([\w\s&/-]+),([\w\s&-]+,\s[\w.]+)\s+(A\n)([\s\S]+?)(EndOfBlock)', text)
-questions = re.findall(r'\n\n(\w{3,}\s[\w\s.]+\n)([\w\s&/-]+),([\w\s&-]+,\s[\w.]+)\s+(Q\n)([\s\S]+?)(EndOfBlock)', text)
-
-dfa = pd.DataFrame(answers, columns=['name', 'title', 'comapny', 'type', 'text', 'ender'])
-dfq = pd.DataFrame(questions, columns=['name', 'title', 'comapny', 'type', 'text', 'ender'])
-
-dfa = dfa.stack().str.strip().unstack()
-dfq = dfq.stack().str.strip().unstack()
+    #remove occurences of ...
+    text = re.sub(r'\.\.\.', '', text)
 
 
 
 
-dfa['count'] = dfa['text'].str.count('EndOFBlock')
-print(dfa)
+    with open('data/out.txt', 'w') as file: 
+        file.write(text)
 
-print(dfq)
 
-dfq.to_excel("data/output.xlsx")
+    with open('data/out.txt') as file: 
+        text = file.read()
 
-#(?<=\s+A\s+)[\w\d]+(?=\.\.\.)'
+    #\n\n(\w+\s[\w\s.]+\n)([\w\s-&/]+),([\w\s-&]+,\s[\w.]+)\s+(A\n)([\s\S]+?)(EndOfBlock)
+    #old regex  : (\sA\n)([\s\S]+?)(EndOfBlock)
+
+    #this one works perfect for netfilx at least, but not apple because they dont have the ,inc. after job titles in the apple one
+    #\n\n(\w{3,}\s[\w\s.]+\n)([\w\s&/-]+),([\w\s&-]+,\s[\w.]+)\s+(Q\n)([\s\S]+?)(EndOfBlock)
+
+    #this one for apple works maybe not netfilx: \n\n([A-Z]\w{2,}\s[\w\s.]+\n)([\w\s&/-]+),([\w\s&,.-]+)\s+(Q\n)([\s\S]+?)(EndOfBlock) this one has glitches in the netfix one now 
+
+
+
+    answers = re.findall(r'\n([\w \.]+)\n([\w, \.&-]+)(A)\n([\s\S]+?)EndOfBlock', text)
+    questions = re.findall(r'\n([\w \.]+)\n([\w, \.&-]+)(Q)\n([\s\S]+?)EndOfBlock', text)
+
+    dfa = pd.DataFrame(answers, columns=['person', 'title', 'type', 'text'])
+    dfq = pd.DataFrame(questions, columns=['person', 'title', 'type', 'text'])
+
+    dfa = dfa.stack().str.strip().unstack()
+    dfq = dfq.stack().str.strip().unstack()
+
+
+
+
+
+    print(dfa)
+    print(dfq)
+
+    dfq.to_excel("data/output.xlsx")
+
+    return dfq, dfa
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import re
 import pandas as pd 
+from earnings_questions import getQAs
+import os
 
-#id : BENTLEY_COL-1674745
-#factset-key:   zy9z0WKHWbxX0o74rQj2CqBjB9Bc4BSc0a2POQY3
+
+def main(): 
+    print('done')
+    parse_earnings(company = 'apple')
+
+def parse_earnings(company): 
+    dfq,dfa = getQAs(company)
+
+    #store the results as csv to avoid redundant computation
+    dfq.to_csv(os.path.join('data', company + '_questions.csv'))
+    dfa.to_csv(os.path.join('data', company + '_answers.csv'))
+
+main()
+
+
